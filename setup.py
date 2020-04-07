646a8fe3a6354c511d39d1e784b87125917bfa7b
Chad Gilbert <chad.s.gilbert@gmail.com>
2020-04-06 22:10:59 -0300

    Trying a bit of a different deploy pipeline.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='makefile2dot',
-    version='0.1.2',
+    version='0.1.3',
     author='Chad Gilbert',
     author_email='chad.s.gilbert@gmail.com',
     description='Create a graphviz graph of a Makefile.',
